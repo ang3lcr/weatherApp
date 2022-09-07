2fbf308edff407b2a451ff4ecf01a2ff31d3c20b
ang3lcr <ang3lcortes@outlook.com>
2022-09-07 03:31:55 +0000

    last-version

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,72 +1,78 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import './Card.css'
 
 function Card() {
   const [weather, setWeather] = useState({});
   const [isCelsius, setIsCelsius] = useState(true);
   const [isFarenheit, setIsFarenheit] = useState(true);
-
-  let weatherUrl;
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
   let apiKey = "953649b7d1a1947b183b4d9462971683"
+  let weatherUrl;
+
+
+
+
   const success = (position) => {
-
-
-
-    function changeUnits(){
-      setIsCelsius(!isCelsius)
-      setIsFarenheit(!isFarenheit)
-    }
-
-
-
-    const latitude = position.coords.latitude
-    const longitude = position.coords.longitude
-    weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
-
+     latitude = position.coords.latitude
+     longitude = position.coords.longitude
   }
 
+useEffect(() => {
+  navigator.geolocation.getCurrentPosition((position) => {
+    setLatitude(position.coords.latitude);
+    setLongitude(position.coords.longitude);
 
-  navigator.geolocation.getCurrentPosition(success)
+  })
+
+}, [])
+
+useEffect(() => {
+
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`).then((res) => {
+    setWeather(res.data);
+  });
+}, []);
 
 
-  useEffect(() => {
-      axios.get(weatherUrl).then((res) => {
-      setWeather(res.data);
-    });
-  }, []);
+function changeUnits(){
+  setIsCelsius(!isCelsius)
+  setIsFarenheit(!isFarenheit)
+}
 
 
-console.log(weather);
+
+
+
+
 
 
   return (
-
-
-    <div className="App">
-
-
+    <div className="card">
     <h1>Weather App</h1>
     <h2>{weather.name}, {weather?.sys?.country}</h2>
 
-    <div className="container">
+
 
     <div className="element-1">
-      <img className="icon-img" src={`http://openweathermap.org/img/wn/${weather.weather[0]?.icon}@2x.png`} />
+      <img className="icon-img" src={`http://openweathermap.org/img/wn/${weather?.weather[0]?.icon}@2x.png`} />
     </div>
+
+    <div className="degrees" >
+            {isCelsius ? weather.main.temp : weather.main.temp * 1.8}{" "}
+            {isCelsius ? "C°" : "F°"}
+        </div>
 
 
     <div className="element-2">
-      <ul>
+      <ul className="list">
         <li>Wind Speed: {weather.wind?.speed}</li>
         <li>Clouds: {weather.clouds?.all}%</li>
         <li>Pressure: {weather.main?.pressure} mb</li>
-        </ul>
+      </ul>
     </div>
-    </div>
-
-
-
-    <button>°F / °C</button>
+    <button onClick={changeUnits}>°F / °C</button>
 
     </div>
 
