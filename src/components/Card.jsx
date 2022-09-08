a6939d0b0051fcd5c7f1b1a0194fab5f5b179ffb
ang3lcr <ang3lcortes@outlook.com>
2022-09-08 02:31:50 +0000

    fixed-bugs

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,44 +8,37 @@ function Card() {
   const [isFarenheit, setIsFarenheit] = useState(true);
   const [latitude, setLatitude] = useState('');
   const [longitude, setLongitude] = useState('');
+
   let apiKey = "953649b7d1a1947b183b4d9462971683"
   let weatherUrl;
 
-
-
-
   const success = (position) => {
-     latitude = position.coords.latitude
-     longitude = position.coords.longitude
+    console.log(position);
+    console.log("exito");
   }
 
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+
+
+    })
+  }, [])
+
+
 useEffect(() => {
-  navigator.geolocation.getCurrentPosition((position) => {
-    setLatitude(position.coords.latitude);
-    setLongitude(position.coords.longitude);
-
-  })
-
-}, [])
-
-useEffect(() => {
-
     axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`).then((res) => {
     setWeather(res.data);
-  });
-}, []);
+      }
+);
+}, [])
 
 
 function changeUnits(){
   setIsCelsius(!isCelsius)
   setIsFarenheit(!isFarenheit)
 }
-
-
-
-
-
-
 
 
   return (
@@ -56,11 +49,12 @@ function changeUnits(){
 
 
     <div className="element-1">
-      <img className="icon-img" src={`http://openweathermap.org/img/wn/${weather?.weather[0]?.icon}@2x.png`} />
+      <img className="icon-img" src={`http://openweathermap.org/img/wn/${weather?.weather?.[0].icon}@2x.png`} />
+
     </div>
 
     <div className="degrees" >
-            {isCelsius ? weather.main.temp : weather.main.temp * 1.8}{" "}
+            {isCelsius ? weather.main?.temp : weather.main?.temp * 1.8}{" "}
             {isCelsius ? "C°" : "F°"}
         </div>
 
